f114d2994fb600d1269dff2b86bed306a666e415
Austin Jones <jonesaustindev@gmail.com>
2023-11-10 14:56:47 -0600

    remove env files from copilot

--- init.lua
+++ init.lua
@@ -306,6 +306,15 @@ require('lazy').setup({
             prev = "<C-[>",
             dismiss = "<Esc>",
           },
+        },
+        filetypes = {
+          sh = function()
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+              -- disable for .env files
+              return false
+            end
+            return true
+          end,
         }
       })
     end,
