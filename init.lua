7d9c1b05e184c159fc72db4634b6e456e623a959
Austin Jones <jonesaustindev@gmail.com>
2023-12-14 14:07:47 -0600

    little update

--- init.lua
+++ init.lua
@@ -172,19 +172,19 @@ require('lazy').setup({
           light = "latte",
           dark = "mocha",
         },
-        transparent_background = true, -- disables setting the background color.
-        show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
-        term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+        term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
-          enabled = false,             -- dims the background color of inactive window
+          enabled = false,              -- dims the background color of inactive window
           shade = "dark",
-          percentage = 0.15,           -- percentage of the shade to apply to the inactive window
+          percentage = 0.15,            -- percentage of the shade to apply to the inactive window
         },
-        no_italic = false,             -- Force no italic
-        no_bold = false,               -- Force no bold
-        no_underline = false,          -- Force no underline
-        styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" },     -- Change the style of comments
+        no_italic = false,              -- Force no italic
+        no_bold = false,                -- Force no bold
+        no_underline = false,           -- Force no underline
+        styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" },      -- Change the style of comments
           conditionals = { "italic" },
           loops = {},
           functions = {},
@@ -299,7 +299,7 @@ require('lazy').setup({
         suggestion = {
           auto_trigger = true,
           keymap = {
-            accept = "<C-i>",
+            accept = "<C-k>",
             accept_word = false,
             accept_line = false,
             next = "<C-]>",
@@ -872,7 +872,14 @@ nvim_lsp.flow.setup {
 
 nvim_lsp.solargraph.setup({
   on_attach = on_attach,
-  capabilities = capabilities
+  capabilities = capabilities,
+  settings = {
+    solargraph = {
+      diagnostics = false,
+      completion = true,
+      formatting = false,
+    },
+  }
 })
 
 -- https://shopify.github.io/ruby-lsp/EDITORS_md.html#Neovim-LSP
